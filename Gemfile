a013815cb7e8eeea848ae503d91954cbbb63da04
Morten Møller Riis <mmr@mv-nordic.com>
2017-06-18 20:56:45 +0200

    Added codecov.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
+gem 'codecov', :require => false, :group => :test
+
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
-# encoding: utf-8
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'simpleidn'
